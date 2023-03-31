ea524c2ed3911f8d92b5f88185fe1c643de90f43
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-03-31 22:50:17 +0530

    github and google authentication completed.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,8 +3,30 @@ import Credentials from "next-auth/providers/credentials";
 import {compare} from 'bcrypt';
 import prismadb from '@/lib/prismadb'
 
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+
+
+// This code sets up a Next.js authentication system using the NextAuth library. It imports the necessary dependencies from various modules - "next-auth/next", "next-auth/providers/credentials", "bcrypt" and a database connection from "prismadb". Then, it exports a default object which is the configuration object for NextAuth.
+
+
+// The configuration object sets up an authentication provider called "Credentials" that enables authentication using an email and password combination. It establishes a user using the Prismadb module and checks whether the email and the password match with the stored user data. The configuration also provides page information for rendering the authentication pages, where a user can sign in. Finally, it sets some environment variables like NODE_ENV, NEXTAUTH_JWT_SECRET & NEXTAUTH_SECRET that are required for the authentication system.
+
+// The code exports the configuration object, which can directly be used by the Next.js application to establish authentication services.
+
 export default NextAuth({
     providers:[
+        GithubProvider({
+            clientId: process.env.GITHUB_ID || '',
+            clientSecret: process.env.GITHUB_SECRET || ''
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
+        }),
         Credentials({
             id: 'credentials',
             name: 'Credentials',
@@ -46,6 +68,7 @@ export default NextAuth({
         signIn: '/auth',
     },
     debug: process.env.NODE_ENV === 'development',
+    adapter: PrismaAdapter(prismadb),
     session: {
         strategy: 'jwt'
     },
