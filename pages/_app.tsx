11e33f5c7e377746c4af6cbbf79d28d5dc73e661
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-04-04 17:35:05 +0530

    favicon and title changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Netflix</title>
+        <link href="/favicon.ico" rel="icon" type="image/x-icon" />
+        <meta property="og:title" content="Netflix-Clone" key="title" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
